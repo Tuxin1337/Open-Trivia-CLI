a5acad2264ebaa86c0b93c7238a4a7547fe855ec
Pascal Möller <pascal1512@gmail.com>
2020-10-28 17:50:33 +0100

    Added get_questions with user input

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ get_token_url = 'https://opentdb.com/api_token.php?command=request'
 base_url = 'https://opentdb.com/api.php?'
 lookup_categorys_url = 'https://opentdb.com/api_category.php'
 #### VARS ####
+token = ""
 amount = 10
 run = True
 logo = '''
@@ -38,11 +39,15 @@ def get_token(url):
     return r.json()['token']
 
 def get_questions(**kwargs):
+    global token
+    if token == "": #check if token exits
+        token = get_token(get_token_url)
+
     url = base_url + f"amount={amount}&token={token}" # Create URL with amount and token
     for key, value in kwargs.items(): # Add custom properties
         url = url + f"&{key}={value}"
     r = make_request(url) #Make request
-
+    print(r.json())
     return r.json()
 
 def print_categorys(): # Print all available categorys
@@ -50,25 +55,64 @@ def print_categorys(): # Print all available categorys
     for category in categorys.json()['trivia_categories']:
         print(f"{category['name']} | {category['id']}, ")
 
+def ask_questions():
+    print("How many questions do you want to get asked? (Default: 10)")
+    amount = input(">> ")
+    print("In which category do you want to get asked? (Default: all)")
+    category = input(">> ")
+    print("Which type of questions do you want to get asked? (boolean, multiple, Default: multiple)")
+    question_type = input(">> ")
+
+    run = True
+
+    if amount == "":
+        amount = 10
+    try:
+        amount = int(amount)
+    except:
+        print('Pls specify an integer for ammount')
+    if amount < 0 or amount > 50: 
+        print('Pls specify amount btw. 1 to 50')
+        run = False
+    if category is not "":
+        try:
+            category = int(category)
+        except:
+            print('Pls specify an integer for category (check: "?")')
+            run = False
+    if question_type == "":
+        question_type = 'multiple'
+    else:
+        question_type = question_type.lower()
+    if question_type not in ['boolean', 'multiple']:
+        print('Use "boolean" or "multiple" for question type')
+        run = False
+
+    if run:
+        get_questions(amount=amount, type=question_type, category=category)
+
 def user_input_handler(user_input):
     user_input = user_input.lower()
     if user_input == '?':
         print_categorys()
     elif user_input in ['v', 'version']:
         print(f"Version: {version}")
+    elif user_input in ['q', 'exit', 'quit']:
+        print("Bye!")
+    elif user_input in ['new', 'n']:
+        ask_questions()
     else:
         print("Help:")
         print('     Write "?" to get a list of categorys')
         print('     Write "q" to exit the programm')
         print('     Write "v" to get the version')
 
-token = get_token(get_token_url)
 
 
 # Main loop
 intro()
 while run:
     user_input = input('>> ')
-    if user_input.lower() in ['q','exit']:
-        break
+    if user_input.lower() in ['q','exit', 'quit']:
+        run = False
     user_input_handler(user_input)
